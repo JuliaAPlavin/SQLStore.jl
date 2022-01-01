df808e432c363a57cfb19daa02ddedf30ef07d62
Alexander Plavin <alexander@plav.in>
2022-01-01 00:00:00 +0000

    implementation

--- src/SQLStore.jl
+++ src/SQLStore.jl
@@ -1,3 +1,179 @@
 module SQLStore
 
+using DBInterface: execute
+using Dates
+import JSON3
+using Tables: rowtable
+using DataPipes
+
+export create_table, table
+
+
+function create_table(db, table_name::AbstractString, T::Type{<:NamedTuple}; constraint=nothing)
+    occursin(r"^\w+$", table_name) || throw("Table name cannot contain special symbols: got $table_name")
+    field_specs = map(fieldnames(T), fieldtypes(T)) do name, type
+        (occursin(r"^\w+$", string(name)) && name != :_rowid_) || throw("Column name cannot contain special symbols: got $name")
+        colspec(name, type)
+    end
+    stmt = """
+    CREATE TABLE $table_name (
+        $(join(field_specs, ",\n    "))
+
+        -- CONSTRAINTS
+        $(isnothing(constraint) ? "" : "," * constraint)
+    )"""
+    execute(db, stmt)
+end
+
+Base.@kwdef struct Table
+    db
+    name::AbstractString
+    schema
+end
+
+function table(db, name::AbstractString)
+    sql_def = first(execute(db, "select * from sqlite_schema where name = :name", (;name))).sql
+    Table(; db, name, schema=parse_sql_to_schema(sql_def))
+end
+
+function parse_sql_to_schema(sql::AbstractString)
+    lines = split(sql, "\n")
+    
+    err = "Table wasn't created by SQLStore.jl, or there is a schema parsing inconsistency"
+    tabname = let line = lines[1]
+        m = match(r"^CREATE TABLE (\w+) \($", lines[1])
+        isnothing(m) && throw(err)
+        m[1]
+    end
+    lines[end] == ")" || throw(err)
+
+    types = [
+        TM
+        for T in [Bool, Int, Float64, String, DateTime, Dict, Any]
+        for TM in (T isa UnionAll || T == Any ? [T] : [T, Union{T, Missing}])
+    ]
+    @p begin
+        lines[2:end]
+        Iterators.takewhile(!isempty(strip(_)))
+        map() do line
+            line = strip(line, [' ', ','])
+            m = match(r"^\w+", line)
+            colname = m.match
+            matching_types = filter(types) do T
+                colspec(colname, T) == line
+            end
+            isempty(matching_types) && throw("No types match definition of column $colname. " * err)
+            length(matching_types) > 1 && throw("Multiple types $matching_types match definition of column $colname")
+            return Symbol(colname) => (
+                type=only(matching_types),
+            )
+        end
+        NamedTuple()
+    end
+end
+
+
+function Base.push!(tbl::Table, row::NamedTuple)
+    fnames = keys(tbl.schema)
+    vals = process_insert_row(row)
+    execute(
+        tbl.db,
+        "insert into $(tbl.name) ($(join(fnames, ", "))) values ($(join(":" .* string.(fnames), ", ")))",
+        vals
+    )
+end
+
+function Base.length(tbl::Table)
+    execute(tbl.db, "select count(*) from $(tbl.name)") |> first |> only
+end
+
+function Base.count(query, tbl::Table)
+    qstr, params = query_to_sql(tbl, query)
+    execute(tbl.db, "select count(*) from $(tbl.name) where $(qstr)", params) |> first |> only
+end
+
+function Base.any(query, tbl::Table)
+    qstr, params = query_to_sql(tbl, query)
+    any(_ -> true, execute(tbl.db, "select * from $(tbl.name) where $(qstr) limit 1", params))
+end
+
+function Base.collect(tbl::Table)
+    map(execute(tbl.db, "select * from $(tbl.name)")) do r
+        process_select_row(tbl.schema, r)
+    end
+end
+
+function Base.filter(query, tbl::Table; limit=nothing)
+    qstr, params = query_to_sql(tbl, query)
+    qres = execute(tbl.db, "select * from $(tbl.name) where $(qstr) $(limit_to_sql(limit))", params)
+    map(qres) do r
+        process_select_row(tbl.schema, r)
+    end
+end
+
+Base.only(query, tbl::Table) = filter(query, tbl; limit=2) |> only
+
+function Iterators.filter(query, tbl::Table)
+    qstr, params = query_to_sql(tbl, query)
+    qres = execute(tbl.db, "select * from $(tbl.name) where $(qstr)", params)
+    Iterators.map(qres) do r
+        process_select_row(tbl.schema, r)
+    end
+end
+
+
+limit_to_sql(lim::Nothing) = ""
+limit_to_sql(lim::Int) = "limit $lim"
+
+query_to_sql(tbl, q::AbstractString) = q, (;)
+query_to_sql(tbl, q::NamedTuple{()}) = "1", (;)  # always-true filter
+query_to_sql(tbl, q::NamedTuple) = @p begin
+    map(keys(q), values(q)) do k, v
+        "$k = :$k"
+    end
+    return join(↑, " and "), process_insert_row(q)
+end
+query_to_sql(tbl, q::Tuple{AbstractString, Vararg}) = first(q), Base.tail(q)
+query_to_sql(tbl, q::Tuple{AbstractString, NamedTuple}) = first(q), last(q)
+
+
+process_insert_row(row) = map(process_insert_field, row)
+process_insert_field(x) = x
+process_insert_field(x::DateTime) = Dates.format(x, dateformat"yyyy-mm-dd HH:MM:SS.sss")
+process_insert_field(x::Dict) = JSON3.write(x)
+
+process_select_row(schema, row) = map(schema, NamedTuple(row)) do sch, val
+    process_select_field(sch.type, val)
+end
+process_select_field(_, x) = x
+process_select_field(::Type{DateTime}, x) = DateTime(x, dateformat"yyyy-mm-dd HH:MM:SS.sss")
+process_select_field(::Type{Dict}, x) = copy(JSON3.read(x))
+
+function colspec(name, T::Type)
+    ct = coltype(T)
+    cc = colcheck(name, T)
+    spec = isempty(cc) ? "$name $ct" : "$name $ct check ($cc)"
+    strip(spec)
+end
+
+# specify integer type as "int", not "integer": we don't want our columns to become rowid
+# this is done for more reliable updating
+coltype(::Type{Bool}) = "int not null"
+coltype(::Type{Int}) = "int not null"
+coltype(::Type{Float64}) = "real not null"
+coltype(::Type{String}) = "text not null"
+coltype(::Type{DateTime}) = "text not null"
+coltype(::Type{Dict}) = "text not null"
+coltype(::Type{Any}) = ""
+coltype(::Type{Union{T, Missing}}) where {T} = replace(coltype(T), " not null" => "")
+
+colcheck(name, ::Type{Bool}) = "$name in (0, 1)"
+colcheck(name, ::Type{Int}) = "typeof($name) = 'integer'"
+colcheck(name, ::Type{Float64}) = "typeof($name) = 'real'"
+colcheck(name, ::Type{String}) = "typeof($name) = 'text'"
+colcheck(name, ::Type{DateTime}) = "typeof($name) = 'text' and $name == strftime('%Y-%m-%d %H:%M:%f', $name)"
+colcheck(name, ::Type{Dict}) = "json_valid($name)"
+colcheck(name, ::Type{Any}) = ""
+colcheck(name, ::Type{Union{T, Missing}}) where {T} = "($(colcheck(name, T))) or $name is null"
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,55 @@
 using SQLStore
+import SQLite
+using Dates: DateTime, now
+using DBInterface: execute
 using Test
 
 @testset begin
+    db = SQLite.DB()
+    create_table(db, "tbl_nt", @NamedTuple{a::Int, b, c::String, d::Union{String, Missing}, x::DateTime, y::Float64})
+    create_table(db, "tbl_pk", @NamedTuple{a::Int, b::String, c::Dict, d::DateTime}; constraint="PRIMARY KEY (a)")
+
+    tbl = table(db, "tbl_pk")
+    @show tbl
+    @test length(tbl) == 0
+    for i in 1:10
+        push!(tbl, (a=i, b="xyz $i", c=Dict("key" => "value $i"), d=DateTime(2020, 1, 2, 3, 4, i)))
+    end
+    @test length(tbl) == 10
+    @test length(collect(tbl)) == 10
+
+    @test count("a = 3", tbl) == 1
+    @test count((;a=3), tbl) == 1
+    @test count("a >= 3", tbl) == 8
+    @test any("a >= 3", tbl)
+
+    let row = (a=3, b="xyz 3", c=Dict(:key => "value 3"), d=DateTime(2020, 1, 2, 3, 4, 3))
+        @test collect(tbl)[3] == row
+
+        @test filter("a = 3", tbl) == [row]
+        @test filter(("a = ?", 3), tbl) == [row]
+        @test filter(("a = ? and b = ?", 3, "xyz 3"), tbl) == [row]
+        @test filter(("a = :xyz", (xyz=3,)), tbl) == [row]
+        @test filter((a=3,), tbl) == [row]
+        @test filter((d=DateTime(2020, 1, 2, 3, 4, 3),), tbl) == [row]
+        @test filter("a <= 3", tbl) == collect(Iterators.filter("a <= 3", tbl))
+
+        @test filter((;), tbl) == collect(tbl)
+        @test filter((;), tbl; limit=3) |> length == 3
+        @test filter((;), tbl; limit=3)[end] == row
+        @test filter((;), tbl; limit=3) == filter("a <= 3", tbl)
+        @test filter((a=3, d=DateTime(2020, 1, 2, 3, 4, 4)), tbl) |> isempty
+        @test filter((c=Dict(:key => "xxx"),), tbl) |> isempty
+
+        @test only((a=3,), tbl) == row
+        @test only((b="xyz 3",), tbl) == row
+        @test only((a=3, d=DateTime(2020, 1, 2, 3, 4, 3)), tbl) == row
+        @test only((c=Dict(:key => "value 3"),), tbl) == row
+        @test_throws ArgumentError only((c=Dict(:key => "xxx"),), tbl)
+        @test_throws ArgumentError only((;), tbl)
+    end
+
+    @test_throws SQLite.SQLiteException push!(tbl, (a=1, b="", c=Dict(), d=now()))
 end
 
 
